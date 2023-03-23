b4e6b4088a215c02adcc37f2aa9d9a12314ea16c
juancruzcarballo <alina.mariel129@gmail.com>
2023-03-22 22:46:24 -0300

    entrega

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -4,22 +4,28 @@ import "./navbar.css";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 
 
 function NavBar() {
     return (
-      // <>
-      // <Navbar bg="light" variant="light" style={{color:'Goldenrod', backgroundColor:'Goldenrod'}}>
- <div>
 
-<NavLink to={`/`}>Home</NavLink>
-<NavLink to={`/category/ipa`}>IPA</NavLink>
-<NavLink to={`/category/rubias`}>Rubias</NavLink>
-<NavLink to={`/category/rojas`}>Rojas</NavLink>
 
-    </div>
 
+
+<div>
+<Navbar bg="light" variant="light" >
+        <Container >
+        <Link to={`/`} > <Navbar.Brand>Home</Navbar.Brand></Link>
+          <Nav className="me-auto" >
+          <Link to={`/category/ipa`}> <Navbar.Brand >IPA</Navbar.Brand></Link>
+          <Link to={`/category/rubias`}> <Navbar.Brand>RUBIAS</Navbar.Brand></Link>
+          <Link to={`/category/rojas`}> <Navbar.Brand>ROJAS</Navbar.Brand></Link>
+          </Nav>
+        </Container>
+      </Navbar>
+</div>
  
 
 
@@ -28,66 +34,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
-
-// import CardWidget from "../CardWidget";
-// import "./navbar.css";
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import { NavLink } from "react-router-dom";
-
-
-
-// function NavBar({changeValue}) {
-//     return (
-//       <>
-//       <Navbar bg="light" variant="light" style={{color:'Goldenrod', backgroundColor:'Goldenrod'}}>
-//       <Container>
-
-//           <NavLink to={"/"}>Home</NavLink>
-//         <NavLink to={"/category/ipa"}>IPA</NavLink>
-//         <NavLink to={"/category/rubias"}>RUBIAS</NavLink>
-//         <NavLink to={"/category/rojas"}>ROJAS</NavLink>
-
-//       </Container>
-//     </Navbar>
-//   </>
-//             //  <div className="navbar">
-//             //     <ul>
-//             //     { /* agregamos boton para ejecutar la funcion cuando le damos click  */}
-
-//             //     <li>
-//             //         <button onClick={changeValue}></button>
-//             //      </li>
-//             //      <li>
-//             //         <button>Inicio</button>
-//             //      </li>
-//             //      <li>
-//             //         <button>Rubias</button>
-//             //      </li>
-
-//             //      <li>
-//             //         <button>Contacto</button>
-//             //      </li>
-
-
-//             //     </ul>
-//             //  <CardWidget />
-//             //  </div>
-
-// /* <Navbar bg="light" variant="light">
-// <Container>
-//   <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-//   <Nav className="me-auto">
-//     <Nav.Link href="#home">Home</Nav.Link>
-//     <Nav.Link href="#features">Features</Nav.Link>
-//     <Nav.Link href="#pricing">Pricing</Nav.Link>
-//   </Nav>
-// </Container>
-// </Navbar> */
-
-//     );
-// }
-
-// export default NavBar;
